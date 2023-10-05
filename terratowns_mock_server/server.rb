75785d7fd5e47892ad81d53488c52e99b248ea69
ITPhilCloud <cubin.phil@googlemail.com>
2023-10-05 19:25:49 +0000

    #40 make sure all our endpoints works correctly

--- terratowns_mock_server/server.rb
+++ terratowns_mock_server/server.rb
@@ -32,11 +32,11 @@ class Home
   # gamers-grotto or
   # cooker-cove
   validates :town, presence: true, inclusion: { in: [
-    `melomaniac-mansion`,  
-    `cooker-cove`,
-    `video-valley`,
-    `the-nomad-pad`,
-    `gamers-grotto`
+    'melomaniac-mansion',  
+    'cooker-cove',
+    'video-valley',
+    'the-nomad-pad',
+    'gamers-grotto'
   ]}
   # visible to all users 
   validates :name, presence: true
@@ -161,7 +161,7 @@ class TerraTownsMockServer < Sinatra::Base
     # ensure our validation checks pass, otherwise
     # return the errors
     unless home.valid?
-      # return the error message back json 
+      # return the error message back as json 
       error 422, home.errors.messages.to_json
     end
 
@@ -217,7 +217,6 @@ class TerraTownsMockServer < Sinatra::Base
     # Validate payload data
     name = payload["name"]
     description = payload["description"]
-    domain_name = payload["domain_name"]
     content_version = payload["content_version"]
 
     unless params[:uuid] == $home[:uuid]
@@ -226,9 +225,9 @@ class TerraTownsMockServer < Sinatra::Base
 
     home = Home.new
     home.town = $home[:town]
+    home.domain_name = $home[:domain_name]
     home.name = name
     home.description = description
-    home.domain_name = domain_name
     home.content_version = content_version
 
     unless home.valid?
@@ -250,8 +249,9 @@ class TerraTownsMockServer < Sinatra::Base
     end
 
     # delete from our mock database
+    uuid = $home[:uuid]
     $home = {}
-    { message: "House deleted successfully" }.to_json
+    { uuid: uuid}.to_json
   end
 end
 
